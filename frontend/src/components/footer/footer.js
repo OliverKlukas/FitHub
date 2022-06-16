381231978c3f0bddc4f2e98ccc1a0aff6f0b2c66
Oliver Klukas <oliver.klukas@tum.de>
2022-06-16 15:20:00 +0200

    added design system typography and replaced headings

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -2,13 +2,13 @@ import {Link, Stack, Typography} from "@mui/material";
 
 export default function Footer() {
     return (<Stack spacing={2} direction="row" marginY={5} justifyContent="space-between">
-            <Link color="#393E46" fontWeight={300} underline="none" href="/terms-and-conditions"> Terms &
+            <Link color="#393E46" fontSize={14} fontWeight={300} underline="none" href="/terms-and-conditions"> Terms &
                 Conditions</Link>
-            <Typography color="#393E46" fontWeight={300}>
+            <Typography variant="caption">
                 Designed using resources from <Link color="inherit" underline="none"
                                                     href="https://www.flaticon.com">flaticon.com</Link> and <Link
                 color="inherit" underline="none" href="https://www.unsplash.com">unsplash.com</Link>
             </Typography>
-            <Link color="#393E46" fontWeight={300} underline="none" href="/about"> About us</Link>
+            <Link color="#393E46" fontSize={14} fontWeight={300} underline="none" href="/about"> About us</Link>
         </Stack>);
 }
