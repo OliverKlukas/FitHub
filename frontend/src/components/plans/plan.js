0ee78f9b171b538595cb124441d7e4f40eb62086
00000000014AAA89 <ge78vof@mytum.de>
2022-06-23 21:23:44 +0200

    code cleaning in payment view, myplans view and components/plans/plan. Formatting done with Prettier to ensure readable code. JSDoc style comments improved. Added comments to all functions and variables used.

--- frontend/src/components/plans/plan.js
+++ frontend/src/components/plans/plan.js
@@ -1,68 +1,73 @@
-import {Stack, Typography} from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import * as React from "react";
-import {HighlightButton} from "../../components/buttons/highlight_button"
-import {StandardButton} from "../../components/buttons/standard_button";
-import {Link as RouterLink} from "react-router-dom";
+import { HighlightButton } from "../../components/buttons/highlight_button";
+import { StandardButton } from "../../components/buttons/standard_button";
+import { Link as RouterLink } from "react-router-dom";
+
+/**
+ * get image src path.
+ *
+ * Adapted from: https://mui.com/material-ui/react-image-list/#CustomImageList.js
+ *
+ * @param image - String path to image resource.
+ * @param width - Number desired width of image.
+ * @param height - Number desired height of image.
+ * @returns {{src: string}} - Returns src variable for img.
+ */
+function srcset(image, width, height) {
+    return {
+        src: `${image}?w=${width}&h=${height}&fit=crop&auto=format`,
+    };
+}
 
 /**
  * my plans view content component including an overview of price, content creator, a download button, a review button that links to the content creators profile and a customer support button.
  *
  * @param item - To be displayed content item, expected to adhere to the database scheme of content.
- * @returns {JSX.Element} - Returns ImageListItem.
+ * @returns {JSX.Element} - Returns content item that will be displayed in the myplans view.
  */
-export default function Plan({item}) {
-
-    function srcset(image, width, height, rows = 1, cols = 1) {
-        return {
-            src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-            srcSet: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format&dpr=2 2x`,
-        };
-    }
-
+export default function Plan({ item }) {
+    // fixed image width and height for the my plans view
     const imgWidth = 200;
     const imgHeight = 200;
 
     return (
-
-        <Stack direction="row" padding={2} spacing={2} justifyContent="space-between" backgroundColor="#EEEEEE" sx={{borderRadius: 5, boxShadow: 5, width:{xs: "100%", lg: "80%", xl: "70%"}}}>
-
+        <Stack
+            direction={{ xs: "column", md: "row" }}
+            padding={2}
+            spacing={2}
+            justifyContent="space-between"
+            backgroundColor="#EEEEEE"
+            sx={{
+                borderRadius: 5,
+                boxShadow: 5,
+                width: { xs: "100%", lg: "90%", xl: "80%" },
+            }}
+        >
             <Stack direction="row" spacing={2}>
-
-                <img
-                    style={{borderRadius: "10px"}}
-                    {...srcset(item.img, imgWidth, imgHeight)}
-                    alt={item.title}
-                />
-
+                <img style={{ borderRadius: "10px" }} {...srcset(item.img, imgWidth, imgHeight)} alt={item.title} />
                 <Stack justifyContent="space-between">
                     <Stack spacing={2}>
-                        <Typography variant="h2" >
-                            {item.title}
-                        </Typography>
-                        <Typography variant="h3" >
-                            Price: {item.price} €
-                        </Typography> 
+                        <Typography variant="h2">{item.title}</Typography>
+                        <Typography variant="h3">Price: {item.price} €</Typography>
                     </Stack>
                     <Stack spacing={2}>
-                        <Typography variant="h4">
-                            by {item.author.name}
-                        </Typography>
-                        <Typography variant="h4">
-                            {item.duration}
-                        </Typography>
+                        <Typography variant="h4">by {item.author.name}</Typography>
+                        <Typography variant="h4">{item.duration}</Typography>
                     </Stack>
-                    
                 </Stack>
-
-            </Stack>                 
-   
-            <Stack justifyContent="space-between">
-                <HighlightButton variant='contained' sx={{width:300}} component={RouterLink} target="_blank" to={"/sample.pdf"} download>Download</HighlightButton>
-                <StandardButton variant="contained" sx={{width:300}} component={RouterLink} to={`/profile`}>Contact customer support</StandardButton>
-                <StandardButton variant="contained" sx={{width:300}}component={RouterLink} to={`/profile`}>Write a review</StandardButton>
             </Stack>
-            
+            <Stack justifyContent="space-between" spacing={2}>
+                <HighlightButton variant="contained" sx={{ width: 300 }} component={RouterLink} target="_blank" to={"/sample.pdf"} download>
+                    Download
+                </HighlightButton>
+                <StandardButton variant="contained" sx={{ width: 300 }} component={RouterLink} to={`/profile`}>
+                    Contact customer support
+                </StandardButton>
+                <StandardButton variant="contained" sx={{ width: 300 }} component={RouterLink} to={`/profile`}>
+                    Write a review
+                </StandardButton>
+            </Stack>
         </Stack>
-
     );
 }
--- frontend/src/views/payment.js
+++ frontend/src/views/payment.js
@@ -1,199 +1,167 @@
-import * as React from 'react';
-import {Box, FormControl, FormControlLabel, Checkbox, Divider, Stack, Typography, Radio, RadioGroup, FormLabel} from "@mui/material";
-import {useNavigate, Link as RouterLink, useParams} from "react-router-dom";
-import {HighlightButton} from "../components/buttons/highlight_button";
-import {content} from "../utils/content";
+import * as React from "react";
+import { Box, FormControlLabel, Checkbox, Divider, Stack, Typography, Radio, RadioGroup, Snackbar } from "@mui/material";
+import { useNavigate, Link as RouterLink, useParams } from "react-router-dom";
+import { HighlightButton } from "../components/buttons/highlight_button";
+import { content } from "../utils/content";
+
+/**
+ * get image src path.
+ *
+ * Adapted from: https://mui.com/material-ui/react-image-list/#CustomImageList.js
+ *
+ * @param image - String path to image resource.
+ * @param width - Number desired width of image.
+ * @param height - Number desired height of image.
+ * @returns {{src: string}} - Returns src variable for img.
+ */
+function srcset(image, width, height) {
+    return {
+        src: `${image}?w=${width}&h=${height}&fit=crop&auto=format`,
+    };
+}
 
 /**
  * Payment page with the most important information about the content item, a link to our Terms and Conditions the customer has to accept and the payment method selection.
  *
  * @returns {JSX.Element} Returns payment page.
  */
+export default function Payment() {
+    // Match url id to content item.
+    let { id } = useParams();
+    // eslint-disable-next-line
+    const item = content.find((item) => item.id == id);
 
-function Payment() {
+    // state for payment method: by default payPal
+    const [paymentMethod, setPaymentMethod] = React.useState("payPal");
 
-  // Match url id to content item.
-  let {id} = useParams();
-  // eslint-disable-next-line
-  const item = content.find((item) => item.id == id);
-
-  const [tncChecked, setTncChecked] = React.useState(false);
-
-  const handleTncChange = (event) => {
-    setTncChecked(event.target.checked);
-  };
-
-  let navigate = useNavigate();
-
-  function handleSubmit(event) {
-    event.preventDefault();  
-      navigate(`/profile`);
-  }
-  
-  function srcset(image, width, height, rows = 1, cols = 1) {
-    return {
-        src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-        srcSet: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format&dpr=2 2x`,
+    // change payment method to newly selected method
+    const handlePaymentChange = (event) => {
+        setPaymentMethod(event.target.value);
     };
-  }
-  
-  return (
-  
-    <Stack spacing={3} marginBottom={10} marginTop={5} alignItems="center">
 
-      <Stack padding={5} backgroundColor="#EEEEEE" sx={{width: {xs: "100%", md: "100%", lg: "80%", xl: "60%"}, borderRadius: 5, boxShadow: 5}}>
+    // state for Terms and Conditions Checkbox
+    const [termsChecked, setTermsChecked] = React.useState(false);
 
-        <Typography variant="h1">
-          {item.title}
-        </Typography>
+    // change acceptance state of the terms and conditions if the checkbox is clicked
+    const handleTermsChange = (event) => {
+        setTermsChecked(event.target.checked);
+    };
 
-        <Divider sx={{mt: 3, bgcolor: "#222831"}}/>
-    
-        <Divider sx={{mt: 1, mb: 3, bgcolor: "#222831"}}/>
+    // Snackbar states for terms and condition checkbox validation
+    const [snackopen, setsnackOpen] = React.useState(false);
 
-        <Stack direction="row" spacing={3}>
+    // close the snackbar
+    const handleSnackClose = () => {
+        setsnackOpen(false);
+    };
 
-          <Box sx={{ display: { xs: 'none', md: 'block' }}}>
+    // handle routing to the myplans view after payment
+    let navigate = useNavigate();
 
-            <img 
-              style={{borderRadius: "8px"}}
-              {...srcset(item.img, 300, 220)}
-              alt="Bild"
-            />        
-                
-          </Box>
+    // check if the terms and conditions are accepted; if accepted go to myplans; if not accepted open the snackbar to display an error message
+    const handleSubmit = () => {
+        if (termsChecked) {
+            navigate(`/myplans/2000`);
+        } else {
+            setsnackOpen(true);
+        }
+    };
 
-          <Box sx={{ width: '100%' }}>
-
-          <Stack>
-
-            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-
-              <Typography variant="h3">
-                Title
-              </Typography>
-
-              <Typography variant="h4">
-
-                {item.title}
-              </Typography>
-
-            </Stack>
-
-            <Divider sx={{my: 2, bgcolor: "#222831"}}/>
-
-            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-
-              <Typography variant="h3">
-                Content Creator
-              </Typography>
-
-              <Typography variant="h4">
-                {item.author.name}
-              </Typography>
-
-            </Stack>
-
-            <Divider sx={{my: 2, bgcolor: "#222831"}}/>
-
-            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-              <Typography variant="h3">
-                Duration
-              </Typography>
-
-              <Typography variant="h4">            
-                {item.duration}
-              </Typography>
-
-            </Stack>
-
-            <Divider sx={{my: 2, bgcolor: "#222831"}}/>
-
-            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-              <Typography variant="h3">
-                Price
-              </Typography>
-
-              <Typography variant="h4">            
-                {item.price}€
-              </Typography>
-
-            </Stack>
-
-            <Divider sx={{mt: 2, mb: 0, bgcolor: "#222831"}}/>         
-
-          </Stack>
-
-          </Box>            
-
-        </Stack>
-
-        <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4} sx={{mt: 2, ml:{xs: 3, md: 0}}}>
-          <Typography variant="h3">
-            Description
-          </Typography>
-
-          <Typography variant="h4">            
-            {item.description}
-          </Typography>
-
-        </Stack>
-
-        <Divider sx={{mt: 3, ml:{xs: 3, md: 0}, bgcolor: "#222831"}}/> 
-
-      </Stack>
-
-      <Box alignItems="flex-start" sx={{width: {xs: "100%", md: "100%", lg: "80%", xl: "60%"}}}>
-        <Typography variant="h2">
-          Payment Options:
-        </Typography>
-      </Box>  
-
-      <Stack paddingLeft={3} backgroundColor="#EEEEEE" sx={{width: {xs: "100%", md: "100%", lg: "80%", xl: "60%"}, borderRadius: 2, boxShadow: 5}}>
-        <RadioGroup defaultValue="payPal">
-          <FormControlLabel control={<Radio />} value="payPal" label="PayPal" />
-        </RadioGroup>         
-      </Stack>
-
-        <Box alignItems="flex-start" sx={{width: {xs: "100%", md: "100%", lg: "80%", xl: "60%"}}}>
-          <Typography variant="h2">
-            Legal Notices:
-          </Typography>
-        </Box>  
-              
-        <Stack paddingLeft={3} direction="row" alignItems="center" backgroundColor="#EEEEEE" sx={{width: {xs: "100%", md: "100%", lg: "80%", xl: "60%"}, borderRadius: 2, boxShadow: 5}}>
-
-          <FormControl component="fieldset" variant="standard">
-
-            <FormLabel sx={{pt: 1}} component="legend"> <Typography variant="h4">Agree to the Terms and Conditions:</Typography></FormLabel>
-
-            <FormControlLabel 
-              control={<Checkbox checked={tncChecked} onChange={handleTncChange}/>}
-              label={
-                <Stack direction="row" spacing={0.5}>
-                  <Typography>I have read the</Typography>
-                  <Typography component={RouterLink} color='inherit' to={`/terms-and-conditions`}>Terms and Conditions </Typography>
+    return (
+        <Stack spacing={3} marginBottom={10} marginTop={5} alignItems="center">
+            <Stack
+                padding={5}
+                backgroundColor="#EEEEEE"
+                sx={{
+                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                    borderRadius: 5,
+                    boxShadow: 5,
+                }}
+            >
+                <Typography variant="h1">{item.title}</Typography>
+                <Divider sx={{ mt: 3, bgcolor: "#222831" }} />
+                <Divider sx={{ mt: 1, mb: 3, bgcolor: "#222831" }} />
+                <Stack direction="row" spacing={3}>
+                    <Box sx={{ display: { xs: "none", md: "block" } }}>
+                        <img style={{ borderRadius: "8px" }} {...srcset(item.img, 300, 220)} alt={item.title} />
+                    </Box>
+                    <Box sx={{ width: "100%" }}>
+                        <Stack>
+                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                <Typography variant="h3">Title</Typography>
+                                <Typography variant="h4">{item.title}</Typography>
+                            </Stack>
+                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                <Typography variant="h3">Content Creator</Typography>
+                                <Typography variant="h4">{item.author.name}</Typography>
+                            </Stack>
+                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                <Typography variant="h3">Duration</Typography>
+                                <Typography variant="h4">{item.duration}</Typography>
+                            </Stack>
+                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                <Typography variant="h3">Price</Typography>
+                                <Typography variant="h4">{item.price}€</Typography>
+                            </Stack>
+                            <Divider sx={{ mt: 2, mb: 0, bgcolor: "#222831" }} />
+                        </Stack>
+                    </Box>
                 </Stack>
-              } 
-            />
-
-          </FormControl>
-          
+                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4} sx={{ mt: 2, ml: { xs: 3, md: 0 } }}>
+                    <Typography variant="h3">Description</Typography>
+                    <Typography variant="h4">{item.description}</Typography>
+                </Stack>
+                <Divider sx={{ mt: 3, ml: { xs: 3, md: 0 }, bgcolor: "#222831" }} />
+            </Stack>
+            <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
+                <Typography variant="h2">Payment Options:</Typography>
+            </Box>
+            <Stack
+                paddingLeft={3}
+                backgroundColor="#EEEEEE"
+                sx={{
+                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                    borderRadius: 2,
+                    boxShadow: 5,
+                }}
+            >
+                <RadioGroup value={paymentMethod} onChange={handlePaymentChange}>
+                    <FormControlLabel control={<Radio />} value="payPal" label="PayPal" />
+                </RadioGroup>
+            </Stack>
+            <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
+                <Typography variant="h2">Legal Notices:</Typography>
+            </Box>
+            <Stack
+                paddingLeft={3}
+                direction="row"
+                alignItems="center"
+                backgroundColor="#EEEEEE"
+                sx={{
+                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                    borderRadius: 2,
+                    boxShadow: 5,
+                }}
+            >
+                <FormControlLabel
+                    control={<Checkbox checked={termsChecked} onChange={handleTermsChange} />}
+                    label={
+                        <Stack direction="row" spacing={0.5}>
+                            <Typography>I have read the</Typography>
+                            <Typography component={RouterLink} color="inherit" to={`/terms-and-conditions`}>
+                                Terms and Conditions{" "}
+                            </Typography>
+                        </Stack>
+                    }
+                />
+            </Stack>
+            <HighlightButton variant="contained" type="submit" onClick={handleSubmit}>
+                Buy Now
+            </HighlightButton>
+            <Snackbar open={snackopen} autoHideDuration={6000} onClose={handleSnackClose} message={"You have to accept FitHub's Terms and Conditions before continuing."} />
         </Stack>
-
-        <Stack>
-
-          <form onSubmit={handleSubmit}>
-          <FormControl>
-            <HighlightButton variant='contained' type="submit">Buy Now</HighlightButton>      
-          </FormControl>
-          </form>
-
-        </Stack>      
-        
-      </Stack>
-
-  );
+    );
 }
-
-export default Payment;
