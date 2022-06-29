43a51eddc3befa7315c775d55732f51139c00a28
00000000014AAA89 <ge78vof@mytum.de>
2022-06-29 09:36:05 +0200

    first try with paypal. Working but not pretty yet.

--- frontend/src/views/payment.js
+++ frontend/src/views/payment.js
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Box, FormControlLabel, Checkbox, Divider, Stack, Typography, Radio, RadioGroup, Snackbar } from "@mui/material";
 import { useNavigate, Link as RouterLink, useParams } from "react-router-dom";
 import { HighlightButton } from "../components/buttons/highlight_button";
+import { PaypalCheckoutButton } from "../components/buttons/paypal_button";
 import { content } from "../utils/content";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 /**
  * get image src path.
@@ -67,101 +69,113 @@ export default function Payment() {
         }
     };
 
+    const product = {
+        description: "Beispiel",
+        price: 10,
+    };
+
     return (
-        <Stack spacing={3} marginBottom={10} marginTop={5} alignItems="center">
-            <Stack
-                padding={5}
-                backgroundColor="#EEEEEE"
-                sx={{
-                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
-                    borderRadius: 5,
-                    boxShadow: 5,
-                }}
-            >
-                <Typography variant="h1">{item.title}</Typography>
-                <Divider sx={{ mt: 3, bgcolor: "#222831" }} />
-                <Divider sx={{ mt: 1, mb: 3, bgcolor: "#222831" }} />
-                <Stack direction="row" spacing={3}>
-                    <Box sx={{ display: { xs: "none", md: "block" } }}>
-                        <img style={{ borderRadius: "8px" }} {...srcset(item.img, 300, 220)} alt={item.title} />
-                    </Box>
-                    <Box sx={{ width: "100%" }}>
-                        <Stack>
-                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-                                <Typography variant="h3">Title</Typography>
-                                <Typography variant="h4">{item.title}</Typography>
+        <PayPalScriptProvider options={{"client-id": "AfSE031dbgxgbMRlT8Ec3OBL00O8o0dShOB_NIHd4vHg5tFVfLRGeERRMV9MZfcxG9_AhXwXZiG7vKRS", "currency": "EUR"}}>
+            <Stack spacing={3} marginBottom={10} marginTop={5} alignItems="center">
+                <Stack
+                    padding={5}
+                    backgroundColor="#EEEEEE"
+                    sx={{
+                        width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                        borderRadius: 5,
+                        boxShadow: 5,
+                    }}
+                >
+                    <Typography variant="h1">{item.title}</Typography>
+                    <Divider sx={{ mt: 3, bgcolor: "#222831" }} />
+                    <Divider sx={{ mt: 1, mb: 3, bgcolor: "#222831" }} />
+                    <Stack direction="row" spacing={3}>
+                        <Box sx={{ display: { xs: "none", md: "block" } }}>
+                            <img style={{ borderRadius: "8px" }} {...srcset(item.img, 300, 220)} alt={item.title} />
+                        </Box>
+                        <Box sx={{ width: "100%" }}>
+                            <Stack>
+                                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                    <Typography variant="h3">Title</Typography>
+                                    <Typography variant="h4">{item.title}</Typography>
+                                </Stack>
+                                <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                    <Typography variant="h3">Content Creator</Typography>
+                                    <Typography variant="h4">{item.author.name}</Typography>
+                                </Stack>
+                                <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                    <Typography variant="h3">Duration</Typography>
+                                    <Typography variant="h4">{item.duration}</Typography>
+                                </Stack>
+                                <Divider sx={{ my: 2, bgcolor: "#222831" }} />
+                                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
+                                    <Typography variant="h3">Price</Typography>
+                                    <Typography variant="h4">{item.price}€</Typography>
+                                </Stack>
+                                <Divider sx={{ mt: 2, mb: 0, bgcolor: "#222831" }} />
                             </Stack>
-                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
-                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-                                <Typography variant="h3">Content Creator</Typography>
-                                <Typography variant="h4">{item.author.name}</Typography>
-                            </Stack>
-                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
-                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-                                <Typography variant="h3">Duration</Typography>
-                                <Typography variant="h4">{item.duration}</Typography>
-                            </Stack>
-                            <Divider sx={{ my: 2, bgcolor: "#222831" }} />
-                            <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4}>
-                                <Typography variant="h3">Price</Typography>
-                                <Typography variant="h4">{item.price}€</Typography>
-                            </Stack>
-                            <Divider sx={{ mt: 2, mb: 0, bgcolor: "#222831" }} />
-                        </Stack>
-                    </Box>
+                        </Box>
+                    </Stack>
+                    <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4} sx={{ mt: 2, ml: { xs: 3, md: 0 } }}>
+                        <Typography variant="h3">Description</Typography>
+                        <Typography variant="h4">{item.description}</Typography>
+                    </Stack>
+                    <Divider sx={{ mt: 3, ml: { xs: 3, md: 0 }, bgcolor: "#222831" }} />
                 </Stack>
-                <Stack direction="row" alignItems="flex-start" justifyContent="space-between" spacing={4} sx={{ mt: 2, ml: { xs: 3, md: 0 } }}>
-                    <Typography variant="h3">Description</Typography>
-                    <Typography variant="h4">{item.description}</Typography>
+                <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
+                    <Typography variant="h2">Payment Options:</Typography>
+                </Box>
+                <Stack
+                    paddingLeft={3}
+                    backgroundColor="#EEEEEE"
+                    sx={{
+                        width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                        borderRadius: 2,
+                        boxShadow: 5,
+                    }}
+                >
+                    <RadioGroup value={paymentMethod} onChange={handlePaymentChange}>
+                        <FormControlLabel control={<Radio />} value="payPal" label="PayPal" />
+                    </RadioGroup>
                 </Stack>
-                <Divider sx={{ mt: 3, ml: { xs: 3, md: 0 }, bgcolor: "#222831" }} />
+                <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
+                    <Typography variant="h2">Legal Notices:</Typography>
+                </Box>
+                <Stack
+                    paddingLeft={3}
+                    direction="row"
+                    alignItems="center"
+                    backgroundColor="#EEEEEE"
+                    sx={{
+                        width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
+                        borderRadius: 2,
+                        boxShadow: 5,
+                    }}
+                >
+                    <FormControlLabel
+                        control={<Checkbox checked={termsChecked} onChange={handleTermsChange} />}
+                        label={
+                            <Stack direction="row" spacing={0.5}>
+                                <Typography>I have read the</Typography>
+                                <Typography component={RouterLink} color="inherit" to={`/terms-and-conditions`}>
+                                    Terms and Conditions{" "}
+                                </Typography>
+                            </Stack>
+                        }
+                    />
+                </Stack>
+                {/*<HighlightButton variant="contained" type="submit" onClick={handleSubmit}>
+                    Buy Now
+                    </HighlightButton>*/}
+
+                <div className="paypal-button-container">
+                    <PaypalCheckoutButton product={product} />
+                </div>
+
+                <Snackbar open={snackopen} autoHideDuration={6000} onClose={handleSnackClose} message={"You have to accept FitHub's Terms and Conditions before continuing."} />
             </Stack>
-            <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
-                <Typography variant="h2">Payment Options:</Typography>
-            </Box>
-            <Stack
-                paddingLeft={3}
-                backgroundColor="#EEEEEE"
-                sx={{
-                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
-                    borderRadius: 2,
-                    boxShadow: 5,
-                }}
-            >
-                <RadioGroup value={paymentMethod} onChange={handlePaymentChange}>
-                    <FormControlLabel control={<Radio />} value="payPal" label="PayPal" />
-                </RadioGroup>
-            </Stack>
-            <Box alignItems="flex-start" sx={{ width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" } }}>
-                <Typography variant="h2">Legal Notices:</Typography>
-            </Box>
-            <Stack
-                paddingLeft={3}
-                direction="row"
-                alignItems="center"
-                backgroundColor="#EEEEEE"
-                sx={{
-                    width: { xs: "100%", md: "100%", lg: "80%", xl: "60%" },
-                    borderRadius: 2,
-                    boxShadow: 5,
-                }}
-            >
-                <FormControlLabel
-                    control={<Checkbox checked={termsChecked} onChange={handleTermsChange} />}
-                    label={
-                        <Stack direction="row" spacing={0.5}>
-                            <Typography>I have read the</Typography>
-                            <Typography component={RouterLink} color="inherit" to={`/terms-and-conditions`}>
-                                Terms and Conditions{" "}
-                            </Typography>
-                        </Stack>
-                    }
-                />
-            </Stack>
-            <HighlightButton variant="contained" type="submit" onClick={handleSubmit}>
-                Buy Now
-            </HighlightButton>
-            <Snackbar open={snackopen} autoHideDuration={6000} onClose={handleSnackClose} message={"You have to accept FitHub's Terms and Conditions before continuing."} />
-        </Stack>
+        </PayPalScriptProvider>
     );
 }
