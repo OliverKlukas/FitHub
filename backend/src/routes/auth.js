69b8388faf20bceca21d3059c191ad4141d316ad
Johannes Loebbecke <johannes.loebbecke@tum.de>
2022-06-29 19:17:37 +0200

    added middleware

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,14 +3,17 @@
 const express = require("express");
 const router = express.Router();
 
+const middleware = require("../middleware")
+
 const AuthController = require("../controllers/auth")
 
-router.post("/register",AuthController.register)
-router.get("/login",AuthController.login)
-router.get("/userdata",AuthController.userdata)  //TODO check middle
-router.post("/logout", AuthController.logout)    //TODO check middleware
-router.post("/addreview", AuthController.addreview)
-router.put("/updatereview", AuthController.updatereview)
-router.delete("/deletereview", AuthController.deletereview)
+router.post("/register",AuthController.register) //register a new user 
+router.get("/login",AuthController.login)   //login
+router.get("/userdata", AuthController.userdata) // display publicly available user data, only works for Content Creators
+router.get("/me", middleware.checkAuthentication, AuthController.me) // displays own user data, needs to be logged in
+router.post("/logout", middleware.checkAuthentication, AuthController.logout)  //logout
+router.post("/addreview", middleware.checkAuthentication, AuthController.addreview) //publishes a review
+router.put("/updatereview", middleware.checkAuthentication, AuthController.updatereview) //update a review
+router.delete("/deletereview", middleware.checkAuthentication, AuthController.deletereview) // delete a review
 
 module.exports = router;
