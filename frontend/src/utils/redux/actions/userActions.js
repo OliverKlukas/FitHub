811af1bc94f3df0d448d3403471b0e0df0e95386
Johannes Loebbecke <johannes.loebbecke@tum.de>
2022-06-30 14:30:59 +0200

    added redux placeholders

---
+++ frontend/src/utils/redux/actions/userActions.js
@@ -0,0 +1,21 @@
+import UserService from "../../../services/userService";
+
+export function login(mail, pass) {
+
+}
+
+export function logout() {
+
+}
+
+export function loginReset() {
+
+}
+
+export function registerCustomer(mail, pass, fname, lname) {
+
+}
+
+export function registerContentCreator(mail, pass, fname, lname, descr, picture) {
+    
+}
