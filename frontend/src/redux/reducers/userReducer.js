3e933c504dd55d779bd6f4115874ba03546a72f7
Johannes Löbbecke <loebbeckejohannes@hotmail.de>
2022-07-01 18:55:26 +0200

    fixed userReduces

---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable require-jsdoc */
+const getUser = () => {
+  if (window.localStorage["jwtToken"]) {
+    const token = window.localStorage["jwtToken"];
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+    const userJson = JSON.parse(window.atob(base64));
+    // if token is expired delete it and return {}
+    // --> User is not logged in anymore.
+    if (userJson.exp > Date.now()) {
+      window.localStorage.removeItem("jwtToken");
+      return {};
+    }
+    return {
+      user: {
+        _id: userJson._id,
+        email: userJson.email,
+        role: userJson.role,
+      },
+    };
+  }
+  return {};
+};
+
+export default function user(state = getUser(), action) {
+  switch (action.type) {
+    case "LOGIN_SUCCESS":
+      return { user: action.user };
+    case "LOGIN_FAILURE":
+      return { error: "Password or username incorrect." };
+    case "LOGIN_RESET":
+      return {};
+    case "LOGOUT":
+      return {};
+    default:
+      return state;
+  }
+}
