dfd1ee746aa4e021d9af05c739c183c92faad41f
Oliver Klukas <oliver.klukas@tum.de>
2022-07-10 18:31:00 +0200

    fixed slide again2

--- frontend/src/components/filters/slide_filter.js
+++ frontend/src/components/filters/slide_filter.js
@@ -30,7 +30,7 @@ export default function SlideFilter({
     // Correct thumb positions if new positions of them surpasses the minDistance.
     if (newPriceRange[1] - newPriceRange[0] < minPriceRange) {
       if (activeThumb === 0) {
-        const clamped = Math.min(newPriceRange[0], 100 - minPriceRange);
+        const clamped = Math.min(newPriceRange[0], 250 - minPriceRange);
         setPriceRange([clamped, clamped + minPriceRange]);
       } else {
         const clamped = Math.max(newPriceRange[1], minPriceRange);
@@ -91,6 +91,7 @@ export default function SlideFilter({
         <Stack alignItems="center" sx={{ mx: 4 }}>
           <Slider
             sx={{ width: 200 }}
+            max={250}
             getAriaLabel={() => "Minimum distance shift"}
             value={priceRange}
             onChange={handleSlide}
